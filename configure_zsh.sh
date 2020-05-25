c16c7b2e43785318d6ca9aa7d841a8c284f6897c
Quentin Delfosse <q.delfosse@gmail.com>
2020-05-25 12:05:33 +0200

    first usefull files: process naming in python programs and auto_zsh_download and configuration

---
+++ configure_zsh.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+yes | apt update
+yes | apt install zsh curl git fonts-hack-ttf
+curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+
+export ZSH="$HOME/.oh-my-zsh"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/junegunn/fzf.git ${ZSH}/custom/plugins/fzf
+${ZSH}/custom/plugins/fzf/install --bin
+git clone https://github.com/Treri/fzf-zsh.git ${ZSH}/custom/plugins/fzf-zsh
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
+cp .p10k.zsh ~/.p10k.zsh
+cp .zshrc ~/.zshrc
+chsh -s /usr/bin/zsh root
+zsh
