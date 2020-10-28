a5fded9834475df4522aa533e7a4878c061a86f8
Quentin Delfosse <q.delfosse@gmail.com>
2020-10-28 10:42:39 +0100

    Added i3 blocks and rofi

--- install_i3.bash
+++ install_i3.bash
@@ -1,9 +1,9 @@
 #!/bin/sh
 # Install status bar
-sudo apt install -y cargo libdbus-1-dev rofi
+sudo apt install -y cargo libdbus-1-dev i3blocks rofi compton nitrogen xdotool dunst
 git clone https://github.com/greshake/i3status-rust
-cd i3status-rust && cargo build --release
 cp -r sound i3 ~/.config
+cp -r i3blocks ~/.config/i3/
 # Optional:
 cp -r target/release/i3status-rs ~/bin/i3status-rs
 
